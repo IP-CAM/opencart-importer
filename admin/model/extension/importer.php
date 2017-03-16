5557373e6f8b886dc5b1bd75f8ee707a159e594f
Myagdeev Vladimir <concertbil@gmail.com>
2017-03-16 18:56:05 +0500

    function preprocessProductFromXML($product_from_xml){} is added to model + adding and updating products in loadXML()

--- admin/model/extension/importer.php
+++ admin/model/extension/importer.php
@@ -1,13 +1,24 @@
 <?php
 class ModelExtensionImporter extends Model
 {
-    public function addOrUpdate(){
+    public function addOrUpdate($data){
         // Функция добавления и обновления товара
+    }
+    public function preprocessProductFromXML($product_from_xml){
+        // Предварительная обработка XML обьекта
+        $preprocessed['name'] = $product_from_xml->Наименование;
+        $preprocessed['id'] = $product_from_xml->Ид;
+        $preprocessed['sku'] = $product_from_xml->Артикул;
+        return $preprocessed; // Макароны по флотски
     }
     public function loadXML($file){
         $this->load->model('catalog/product');
         $xml =  simplexml_load_file($file);
-        return $xml->Каталог->Товары;
+
         // КоммерческаяИнформация->Каталог->Товары
+        foreach ($xml->Каталог->Товары as $product_from_xml){
+            $this->addOrUpdate($this->preprocessProductFromXML($product_from_xml));
+        }
+
     }
 }
