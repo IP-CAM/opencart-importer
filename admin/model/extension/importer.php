eb4fa70e1ad93174610ad6c65ae5fe6b98ad3bd6
Myagdeev Vladimir <concertbil@gmail.com>
2017-03-17 16:58:31 +0500

    Added function getLanguageIDByCode(){..} into model.

--- admin/model/extension/importer.php
+++ admin/model/extension/importer.php
@@ -4,6 +4,7 @@ class ModelExtensionImporter extends Model
     public function addOrUpdateProduct($data){
         // Функция добавления и обновления товара
         $this->load->model('catalog/product');
+        var_dump($data);
     }
     public function preprocessProductFromXML($product_from_xml){
         // Предварительная обработка XML обьекта
@@ -23,8 +24,18 @@ class ModelExtensionImporter extends Model
         return $preprocessed;
     }
 
-    public function loadXML($file){
+    public function getLanguageIDByCode($code = 'ru-ru'){
+        // Получение language_id по коду языка, русский язык по умолчанию
+        $query = $this->db->query("SELECT language_id FROM " . DB_PREFIX . "language WHERE code='$code';");
+        if ($query->num_rows) {
+            $data = $query->row;
+            return $data['language_id'];
+        }
+    }
+
+    public function loadXML($file, $lang_code = 'ru-ru'){
         $xml =  simplexml_load_file($file);
+        $language_id = $this->getLanguageIDByCode('ru-ru');
 
         // КоммерческаяИнформация->Каталог->Товары
         foreach ($xml->Каталог->Товары as $product_from_xml){
